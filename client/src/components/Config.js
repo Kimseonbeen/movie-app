d134dcb3165f9b59aa71dc0e18d2e9394463c5df
김선빈 <ksb9242@gmail.com>
2021-05-20 22:02:18 +0900

    movie-app

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '059da6ff72af9fceb3f7f0fdb2511281';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
